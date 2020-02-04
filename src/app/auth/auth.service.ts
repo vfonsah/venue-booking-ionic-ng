a763977fd48d88b4c0788b9880c6d52428a4d84b
vfonsah <vitalis.fonsah@gmail.com>
2020-02-05 00:03:34 +0100

    the auth service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  // tslint:disable-next-line: variable-name
+  private _userIsAuthenticated = false;
+
+  get userIsAuthenticated() {
+    return this._userIsAuthenticated;
+  }
+
+  logIn(){
+    this._userIsAuthenticated = true;
+  }
+
+  logOut(){
+    this._userIsAuthenticated = false;
+  }
+  constructor() { }
+}
