0114cf1d4eff0145b73d0f8b17bd64429704881b
vfonsah <vitalis.fonsah@gmail.com>
2020-02-21 06:46:44 +0100

    close slide onClick & specify slide side start

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IonItemSliding } from '@ionic/angular';
+import {Router} from '@angular/router';
+
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
 
@@ -11,13 +14,15 @@ export class OffersPage implements OnInit {
 
   offers: Place[];
 
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService, private router: Router) { }
 
   ngOnInit() {
     this.offers = this.placesService.places;
   }
 
-  onEdit(offerId: string){
+  onEdit(offerId: string, slidingItem: IonItemSliding){
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
     console.log('Editing Item', offerId);
   }
 }
