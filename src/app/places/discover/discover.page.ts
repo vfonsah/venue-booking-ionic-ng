f4d2c844fb22ee8d90caebae74fee3908eb6bb3f
vfonsah <vitalis.fonsah@gmail.com>
2020-02-21 08:48:35 +0100

    segmented btns on discover page

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import {SegmentChangeEventDetail} from '@ionic/core'
+
 import { PlacesService } from '../places.service';
 import {Place} from '../place.model';
-import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-discover',
@@ -22,5 +24,9 @@ export class DiscoverPage implements OnInit {
 
   onOpenMenu() {
     this.menuCtrl.toggle();
+  }
+
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    console.log(event.detail);
   }
 }
