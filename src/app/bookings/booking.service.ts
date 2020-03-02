2cc46cafbb1281dc0afc9de1ace0e0f579e9d4c6
vfonsah <vitalis.fonsah@gmail.com>
2020-03-02 19:44:18 +0100

    delete(unsubscribe) place booking

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -107,12 +107,18 @@ export class BookingService {
   }
 
   cancelBooking(bookingId: string) {
-    return this.bookings.pipe(
-      take(1),
-      delay(1000),
-      tap(bookings => {
-        this._bookings.next(bookings.filter(b => b.id !== bookingId));
-      })
-    );
+    return this.http
+      .delete(
+        `https://ionic-venue-booking-ng.firebaseio.com/place-bookings/${bookingId}.json`
+      )
+      .pipe(
+        switchMap(() => {
+          return this.bookings;
+        }),
+        take(1),
+        tap(bookings => {
+          this._bookings.next(bookings.filter(b => b.id !== bookingId));
+        })
+      );
   }
 }
